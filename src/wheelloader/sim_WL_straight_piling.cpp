54ba5f2d94a0e8f52d7674cbe926e56efc5a1824
victor.bertolazzo <bertolazzo.victor@gmail.com>
2017-09-26 20:28:49 +0200

    Add vehicle features to the complete wheel loader simulation.

--- src/wheelloader/sim_WL_straight_piling.cpp
+++ src/wheelloader/sim_WL_straight_piling.cpp
@@ -45,12 +45,20 @@
 #include "chrono_parallel/physics/ChSystemParallel.h"
 #include "chrono_parallel/collision/ChBroadphaseUtils.h"
 
+// Chrono::Vehicle header files
+#include "chrono_vehicle/ChVehicleModelData.h"
+#include "chrono_vehicle/driver/ChDataDriver.h"
+#include "chrono_vehicle/terrain/RigidTerrain.h"
+	// Driver header files
+#include "chrono_vehicle/driver/ChPathFollowerDriver.h"
+
 #ifdef CHRONO_OPENGL
 #include "chrono_opengl/ChOpenGLWindow.h"
 #endif
 
 // Custom functions 
 #include "utilities/ZBarMechanism.h"
+
 
 // Articulated Subsystems
 #include "subsystems/Articulated_Front.h"
@@ -97,6 +105,13 @@ ChVector<> inertia_g = 0.4 * mass_g * r_g * r_g * ChVector<>(1, 1, 1);
 double coh_force = CH_C_PI * r_g * r_g * coh_pressure;
 
 // =============================================================================
+// Specification of the terrain model
+// =============================================================================
+double terrainHeight = 0;
+double terrainLength = 1000.0;  // size in X direction
+double terrainWidth = 1000.0;   // size in Y direction
+
+// =============================================================================
 // Specification of the vehicle model
 // =============================================================================
 
@@ -118,7 +133,7 @@ int threads = 20;
 bool thread_tuning = false;
 
 // Total simulation duration.
-double time_end = 1;
+double time_end = 500;
 
 // Solver parameters
 double time_step = 1e-3;
@@ -142,6 +157,15 @@ const std::string out_dir = "../WL_NSC";
 const std::string pov_dir = out_dir + "/POVRAY";
 // Frame per Second value: povray output setting.
 int out_fps = 60;
+
+// =============================================================================
+//	PATH FOLLOWER FILES
+// =============================================================================
+
+// Input file names for the path-follower driver model
+std::string steering_controller_file("generic/driver/SteeringController.json");
+std::string speed_controller_file("generic/driver/SpeedController.json");
+std::string path_file("paths/straight10km.txt");
 
 // -----------------------------------------------------------------------------
 // Generate postprocessing output with current system state.
@@ -366,23 +390,23 @@ int main(int argc, char* argv[]){
 	// --------------------------
 	// --------------------------
 	// Create the ground(terrain)
-	auto ground = CreateGround(system);
+			//auto ground = CreateGround(system);
 	// Create the sandpile(spheres pyramid)--Calculate computational time to build it.
 	ChVector<> hdims(2., 2., 0.);
 	int start_s = clock();
-	//auto sandpile = CreateSandpile(system, material_terrain, hdims);
+			//auto sandpile = CreateSandpile(system, material_terrain, hdims);
 	int stop_s = clock();
 	std::cout << "Sandpile creation computational time: " << (stop_s - start_s) / double(CLOCKS_PER_SEC) * 1000 << std::endl;
-	//std::cout << "Number of created particles: " << sandpile.getTotalNumBodies() << std::endl;
+			//std::cout << "Number of created particles: " << sandpile.getTotalNumBodies() << std::endl;
 	//Create the loader(mechanism only, with a fake chassis)
-	MyWheelLoader* loader = CreateLoader(system);
+			//MyWheelLoader* loader = CreateLoader(system);
 	// Create the vehicle(the whole wheel loader except the mechanism)
 	// --------------------------
 	// --------------------------
 	// Create the front side
-	Articulated_Front front_side(false);
+	Articulated_Front front_side(system);// it avoids to create two concurrent systems.
 	front_side.Initialize(ChCoordsys<>(initLoc, initRot));
-	front_side.SetChassisVisualizationType(VisualizationType::PRIMITIVES);
+	front_side.SetChassisVisualizationType(VisualizationType::MESH);
 	front_side.SetSuspensionVisualizationType(VisualizationType::PRIMITIVES);
 	front_side.SetSteeringVisualizationType(VisualizationType::PRIMITIVES);
 	front_side.SetWheelVisualizationType(VisualizationType::NONE);
@@ -393,18 +417,22 @@ int main(int argc, char* argv[]){
 	rear_side.SetSuspensionVisualizationType(VisualizationType::PRIMITIVES);
 	rear_side.SetWheelVisualizationType(VisualizationType::NONE);
 
-	////////////// Create the terrain
-	////////////RigidTerrain terrain(front_side.GetSystem());
-	////////////terrain.SetContactFrictionCoefficient(0.9f);
-	////////////terrain.SetContactRestitutionCoefficient(0.01f);
-	////////////terrain.SetContactMaterialProperties(2e7f, 0.3f);
-	////////////terrain.SetColor(ChColor(0.5f, 0.5f, 0.5f));
-	////////////terrain.SetTexture(vehicle::GetDataFile("terrain/textures/tile4.jpg"), 200, 200);
-	////////////terrain.Initialize(terrainHeight, terrainLength, terrainWidth);
+	// Create the terrain
+	RigidTerrain terrain(system);
+	terrain.SetContactFrictionCoefficient(0.9f);
+	terrain.SetContactRestitutionCoefficient(0.01f);
+	terrain.SetContactMaterialProperties(2e7f, 0.3f);
+	terrain.SetColor(ChColor(0.5f, 0.5f, 0.5f));
+	terrain.SetTexture(vehicle::GetDataFile("terrain/textures/tile4.jpg"), 200, 200);
+	terrain.Initialize(terrainHeight, terrainLength, terrainWidth);
+	//terrain.GetGroundBody()->GetCollisionModel()->ClearModel();
+	//auto ground = std::shared_ptr<ChBody>(terrain.GetGroundBody());
+	//utils::AddBoxGeometry(ground.get(), ChVector<>(terrainLength, terrainWidth, .25), ChVector<>(0, 0, -.25),ChQuaternion<>(1, 0, 0, 0), true);
+	//ground->GetCollisionModel()->BuildModel();
 
 	// Create and initialize the powertrain system
 	// Vehicle is 4WD indeed
-	auto driveline = std::make_shared<WL_SimpleDriveline4WD>("driveline");//WL_Driveline4WD works.
+	auto driveline = std::make_shared<WL_SimpleDriveline4WD>("WL_driveline");//WL_Driveline4WD works.
 	ChSuspensionList suspensions; suspensions.resize(2);
 	suspensions[0] = front_side.GetSuspension(0); suspensions[1] = rear_side.GetSuspension(0);
 	std::vector<int> driven_susp_indexes(2);//(ignored)
@@ -465,7 +493,36 @@ int main(int argc, char* argv[]){
 	// --------------------------
 	// Set Chassis and Piston motion law.
 	// --------------------------
-	SetPistonsMovement(system, loader);
+		//SetPistonsMovement(system, loader);
+
+	// --------------------------
+	// Set Path Follower settings.
+	// --------------------------
+	auto path = ChBezierCurve::read(vehicle::GetDataFile(path_file));
+	//auto path = ChBezierCurve::read(vehicle::GetDataFile("paths/WL_Path_First_Segment.dat"));// Uncomment to select real WL path.
+	ChPathFollowerDriver driver(front_side, vehicle::GetDataFile(steering_controller_file),	vehicle::GetDataFile(speed_controller_file), path, "my_path", 3.0);
+	//ChDataDriver driver(front_side, "C:/Users/victo/Documents/chrono_fork_victor-build/bin/data/WL_Man_NoSteer.dat");
+	driver.Initialize();
+
+	// --------------------------
+	// Create Selected Gear Time Series-- Test file : WL_SelectedGear.dat
+	// --------------------------
+	std::vector<TimeSeries> SelectedGear;
+	ReadPressureFile("../data/WL_SelectedGear.dat", SelectedGear);
+	auto gear = std::make_shared<ChFunction_Recorder>();
+	for (int i = 0; i < SelectedGear.size(); i++){
+		gear->AddPoint(SelectedGear[i].mt, SelectedGear[i].mv);
+	}
+
+	// --------------------------
+	// Create Desired Speed Time Series -- Test file WL_DesiredSpeed.dat
+	// --------------------------
+	std::vector<TimeSeries> DesiredSpeed;
+	ReadPressureFile("../data/WL_DesiredSpeedSmoothed.dat", DesiredSpeed);
+	auto target_speed = std::make_shared<ChFunction_Recorder>();
+	for (int i = 0; i < DesiredSpeed.size(); i++){
+		target_speed->AddPoint(DesiredSpeed[i].mt, DesiredSpeed[i].mv);
+	}
 
 	// --------------------------
 	// Initialize OpenGL window..
@@ -473,57 +530,143 @@ int main(int argc, char* argv[]){
 #ifdef CHRONO_OPENGL
 	opengl::ChOpenGLWindow& gl_window = opengl::ChOpenGLWindow::getInstance();
 	gl_window.Initialize(1280, 720, "Wheel Loader-Piling Manouevre", system);
-	gl_window.SetCamera(ChVector<>(0, -10, 0), ChVector<>(7.5, 0, 0), ChVector<>(0, 0, 1));
+	gl_window.SetCamera(ChVector<>(0, -15, 0), ChVector<>(0.0, 0, 0), ChVector<>(0, 0, 1));
 	gl_window.SetRenderMode(opengl::WIREFRAME);
 #endif
 
 
 
+	// --------------------------
 	// Run simulation for specified time
+	// --------------------------
 	int num_steps = std::ceil(time_end / time_step);
 	int out_steps = std::ceil((1 / time_step) / out_fps);
 	int out_frame = 0;
 	double time = 0;
 
+	// --------------------------
+	// Inter-module communication data variables.
+	// --------------------------
+	TireForces tire_front_forces(2);
+	TireForces tire_rear_forces(2);
+	double driveshaft_speed;
+	double powertrain_torque;
+	double throttle_input;
+	double steering_input;
+	double braking_input;
+	double gear_input;
+	// --------------------------
+	// Output Loader data variables.
+	// --------------------------
+	double pos_lift; double pos_tilt;
+	double ang_lift;double ang_tilt;
+	double fp_lift; double fp_tilt;
 
+
+	// --------------------------
+	// Print System Hierarchy.
+	// --------------------------
+	system->ShowHierarchy(GetLog());
+
+	// --------------------------
 	// Simulation Loop--The Loop is the standard for Chrono::Vehicle demos.
+	// --------------------------
+	// --------------------------
+	// --------------------------
+	// --------------------------
 	for (int i = 0; i < num_steps; i++) {
 
-		// Collect output data from modules.
+		// --------------------------
+		// Collect output data from modules (for inter-module communication)
+		// --------------------------
+		throttle_input = driver.GetThrottle();
+		steering_input = driver.GetSteering();
+		braking_input = driver.GetBraking();
+		powertrain_torque = powertrain.GetOutputTorque();
+			gear_input = (int)gear->Get_y(time);
 
-		double pos_lift = loader->lin_ch2lift->Get_dist_funct()->Get_y(time) + loader->lin_ch2lift->Get_lin_offset();
-		double pos_tilt = loader->lin_lift2rod->Get_dist_funct()->Get_y(time) + loader->lin_ch2lift->Get_lin_offset();
+		tire_front_forces[0] = tire_FL->GetTireForce();tire_front_forces[1] = tire_FR->GetTireForce();
+		tire_rear_forces[0] = tire_RL->GetTireForce();tire_rear_forces[1] = tire_RR->GetTireForce();
 
-		double ang_lift = loader->GetLiftArmPitchAngle();
-		double ang_tilt = loader->GetTiltBucketPitchAngle();
+		driveshaft_speed = driveline->GetDriveshaftSpeed();
 
-		double fp_lift = loader->GetReactLiftForce();
-		double fp_tilt = loader->GetReactTiltForce();
+		WheelState wheel_FL = front_side.GetWheelState(FRONT_LEFT);WheelState wheel_FR = front_side.GetWheelState(FRONT_RIGHT);
+		WheelState wheel_RL = rear_side.GetWheelState(FRONT_LEFT);WheelState wheel_RR = rear_side.GetWheelState(FRONT_RIGHT);
 
+		// --------------------------
+		// Collect output data from wheel loader module.
+		// --------------------------
+
+		//pos_lift = loader->lin_ch2lift->Get_dist_funct()->Get_y(time) + loader->lin_ch2lift->Get_lin_offset();
+		//pos_tilt = loader->lin_lift2rod->Get_dist_funct()->Get_y(time) + loader->lin_ch2lift->Get_lin_offset();
+
+		//ang_lift = loader->GetLiftArmPitchAngle();
+		//ang_tilt = loader->GetTiltBucketPitchAngle();
+
+		//fp_lift = loader->GetReactLiftForce();
+		//fp_tilt = loader->GetReactTiltForce();
+		//
+		// --------------------------
+		// Update modules (process inputs from other modules)--Synchronize method call.
+		// --------------------------
+		time = front_side.GetSystem()->GetChTime();
+		driver.Synchronize(time);
+		terrain.Synchronize(time);
+		tire_FL->Synchronize(time, wheel_FL, terrain);tire_FR->Synchronize(time, wheel_FR, terrain);
+		tire_RL->Synchronize(time, wheel_RL, terrain);tire_RR->Synchronize(time, wheel_RR, terrain);
+
+			// Select Direction(it should be called only at changes)
+
+			// Select Gear
+			powertrain.SetSelectedGear(gear_input);// Should it be a function of time as it is, ge(t), or position,ge(x,y) or speed,ge(v) ?
+		powertrain.Synchronize(time, throttle_input, driveshaft_speed);
+
+		driveline->Synchronize(powertrain_torque);//Is the time info needed?no
+
+		front_side.Synchronize(time, steering_input, braking_input, powertrain_torque, tire_front_forces);// powertrain_torque will be ignored since driveline is outside of ArticulatedVehicle(function overriding)
+		rear_side.Synchronize(time, steering_input, braking_input, tire_rear_forces);
+			driver.SetDesiredSpeed(target_speed->Get_y(time + 0.5));//Change target speed for successive Advance method.In this way speed is a function of time , v(t).// needed to translate in a function of position v(x,y), this is a path follower not a traj follower.// "look ahead" speed
+
+		// --------------------------
+		// Advance simulation for one timestep for all modules
+		// --------------------------
+		driver.Advance(time_step);
+		terrain.Advance(time_step);
+		tire_FL->Advance(time_step); tire_FR->Advance(time_step);
+		tire_RL->Advance(time_step); tire_RR->Advance(time_step);
+		powertrain.Advance(time_step);
+
+		// --------------------------
+		// Perform time step integration.
+		// --------------------------
+
+		//system->DoStepDynamics(time_step);
+		front_side.Advance(time_step);// This call contains "system->DoStepDynamics(time_step)"
+
+		// --------------------------
 		// Save output data from modules on CSV writer.
+		// --------------------------
 		csv << pos_lift << pos_tilt;
 		csv << ang_lift << ang_tilt;
 		csv << fp_lift << fp_tilt;
 		csv << std::endl;
 
 
+		// --------------------------
+		// Write output info on CSV file.
+		// --------------------------
 		if (i % out_steps == 0) {
 			OutputPOVRayData(system, out_frame, time);
 			out_frame++;
 
 			csv.write_to_file(out_dir + "/output.dat");
-
 		}
-		system->DoStepDynamics(time_step);
 		time += time_step;
 
-
+// Render the frame
 #ifdef CHRONO_OPENGL
 		if (gl_window.Active())
-		{
-			gl_window.Render();
-			//std::cin.get();
-		}
+		{gl_window.Render();}
 		else
 			break;
 #endif
